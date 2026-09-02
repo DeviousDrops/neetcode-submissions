d2427c44e7447baa749f1b972a904ab2c6ed019b
I Kartik Reddy <ikartik.reddy@gmail.com>
2026-09-02 11:43:19 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if(nums.size()==1)
+            return nums[0];
+        vector<int> res(nums.size());
+        res[0]=nums[0];
+        res[1]=max(res[0],nums[1]);
+        for(int i=2;i<nums.size();i++){
+            res[i]=max(res[i-1],nums[i]+res[i-2]);
+        }    
+        return res[nums.size()-1];
+        
+    }
+};
